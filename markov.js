3a48fc721459ddcfa0bd4d3ff3b4c637f6d1b075
jeanne <novasquared@yahoo.com>
2021-09-28 18:22:35 -0500

    created constructor and makeChains functions and planned out getText

--- markov.js
+++ markov.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Textual markov chain generator */
 
 
@@ -7,7 +8,9 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    console.log(words);
+    this.words = words;
+    this.makeChains();
   }
 
   /** set markov chains:
@@ -16,13 +19,35 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // MORE CODE HERE
-  }
 
+    let chains = {};
+
+    for (let i = 0; i < this.words.length; i++) {
+      if (!chains[this.words[i]]) {
+        chains[this.words[i]] = [this.words[i + 1] || null]
+      } else {
+        chains[this.words[i]].push(this.words[i + 1] || null);
+      }
+    }
+    console.log(chains);
+  }
 
   /** return random text from chains */
 
   getText(numWords = 100) {
-    // MORE CODE HERE
+    // construct text from make chains.
+    //get length chains.keys.
+    // generate a random number to pick first word
+    // find the length of the array for the key we picked
+    // generate a random number to pick the next word if length > 1
+    // repeat until we get a null to end the sentence.
+    let wordCount = 0;
+    while (wordCount < numWords) {
+      // keep constructing sentences counting each word
+      // when we hit null, add a . and a space
+      
+    } 
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat");
