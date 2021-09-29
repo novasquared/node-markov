54e6d2d9b937b6cdae579c892e757fa0003da08b
samau3 <samau415@gmail.com>
2021-09-28 17:37:32 -0700

    completed basic markov algorithm behavior

--- markov.js
+++ markov.js
@@ -8,9 +8,10 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    console.log(words);
+    // console.log(words);
     this.words = words;
-    this.makeChains();
+    let wordChains = this.makeChains();
+    this.wordChains = wordChains;
   }
 
   /** set markov chains:
@@ -29,25 +30,62 @@ class MarkovMachine {
         chains[this.words[i]].push(this.words[i + 1] || null);
       }
     }
-    console.log(chains);
+    return chains;
   }
 
   /** return random text from chains */
 
+
   getText(numWords = 100) {
+
     // construct text from make chains.
     //get length chains.keys.
     // generate a random number to pick first word
     // find the length of the array for the key we picked
     // generate a random number to pick the next word if length > 1
     // repeat until we get a null to end the sentence.
-    let wordCount = 0;
-    while (wordCount < numWords) {
-      // keep constructing sentences counting each word
-      // when we hit null, add a . and a space
-      
-    } 
+
+    const numKeys = Object.keys(this.wordChains).length;
+
+    let text = '';
+    let wordsCount = 0;
+    while (wordsCount < numWords) {
+      let words = [];
+      let rand = Math.floor(Math.random() * numKeys)
+      // console.log("rand", rand)
+      let firstWord = Object.keys(this.wordChains)[rand]
+      // console.log("firstWord", firstWord)
+      words.push(firstWord)
+      wordsCount++
+
+      // another loop
+      // once you pick the "nextWord", that becomes the new first word
+      // repeat this loop until null
+      // once null, repeat the process of picking a new "firstWord"
+
+      while (firstWord !== null) {
+        let wordArrLength = this.wordChains[firstWord].length
+        let nextWordRand = Math.floor(Math.random() * wordArrLength)
+        let nextWord = this.wordChains[firstWord][nextWordRand]
+        if (nextWord !== null) {
+          words.push(nextWord);
+          wordsCount++;
+        }
+        if (wordsCount > numWords) {
+          break
+        }
+        firstWord = nextWord
+      }
+      let sentence = words.join(" ") + ". ";
+      text += sentence;
+    }
+    return text;
+
   }
 }
 
+
 let mm = new MarkovMachine("the cat in the hat");
+let machineText = mm.getText();
+console.log(machineText)
+
